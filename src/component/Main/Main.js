15523f3fb072f47ed096e822dbf94d0a1f2a2292
BikramChakraborty <chakrabortybikram12345@gmail.com>
2025-09-11 19:23:33 +0530

    update filenew

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Main/Main.css';
 import Cards from '../Cards/Cards';
 import DataObject from '../../DataObject';
 
 export default function Main() {
-
+  const [count, setCounter] = useState(0);
+  const countClick = () => {
+    setCounter(count + 1);
+  }
+  const countClickDecrese = () => {
+    setCounter(count - 1);
+  }
   return (
     <div className="content">
       <div className="section-title">
@@ -17,7 +23,14 @@ export default function Main() {
       </div>
 
       <section className="grid" aria-label="Card grid">
-        <Cards cardProps = {DataObject.cardObj} />
+        <Cards cardProps={DataObject.cardObj} />
+        <div className='use-state-exm'>
+          <button onClick={countClick}>
+            +
+          </button>
+          <span>Count Me: {count}</span>
+          <button onClick={countClickDecrese}>-</button>
+        </div>
       </section>
     </div>
   )
